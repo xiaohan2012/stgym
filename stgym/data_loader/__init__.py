04f3c307c7df056bda7ad68091bb89c3df0d15c2
Han Xiao <hanxiao@Hans-MacBook-Pro.local>
2025-04-07 10:07:19 +0300

    test tl module

--- stgym/data_loader/__init__.py
+++ stgym/data_loader/__init__.py
@@ -1,4 +1,7 @@
 import torch_geometric.datasets as pg_datasets
+from torch.utils.data import DataLoader
+from torch_geometric.data.lightning.datamodule import LightningDataModule
+
 from stgym.config_schema import DataLoaderConfig
 from torch.utils.data import random_split
 from torch_geometric.loader import DataLoader
@@ -17,3 +20,28 @@ def create_loader(cfg: DataLoaderConfig):
     val_loader = DataLoader(val_ds, batch_size=cfg.batch_size, shuffle=False)
     test_loader = DataLoader(test_ds, batch_size=cfg.batch_size, shuffle=False)
     return train_loader, val_loader, test_loader
+
+
+class STDataModule(LightningDataModule):
+    r"""A :class:`pytorch_lightning.LightningDataModule` for handling data
+    loading routines in GraphGym.
+
+    This class provides data loaders for training, validation, and testing, and
+    can be accessed through the :meth:`train_dataloader`,
+    :meth:`val_dataloader`, and :meth:`test_dataloader` methods, respectively.
+    """
+
+    def __init__(self, cfg: DataLoaderConfig):
+        self.loaders = create_loader(cfg)
+        super().__init__(has_val=True, has_test=True)
+
+    def train_dataloader(self) -> DataLoader:
+        return self.loaders[0]
+
+    def val_dataloader(self) -> DataLoader:
+        # better way would be to test after fit.
+        # First call trainer.fit(...) then trainer.test(...)
+        return self.loaders[1]
+
+    def test_dataloader(self) -> DataLoader:
+        return self.loaders[2]
--- tests/data_loader/test_loader.py
+++ tests/data_loader/test_loader.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 from torch_geometric.datasets import BA2MotifDataset
 
 from stgym.config_schema import DataLoaderConfig
-from stgym.data_loader import create_loader, load_dataset
+from stgym.data_loader import create_loader, load_dataset, STDataModule
 
 
 @pytest.fixture
@@ -21,3 +21,10 @@ def test_create_loader(mock_cfg):
     assert len(loaders) == 3
     for loader in loaders:
         assert isinstance(loader, DataLoader)
+
+
+def test_tl_module_init(mock_cfg):
+    mod = STDataModule(mock_cfg)
+    assert isinstance(mod.train_dataloader(), DataLoader)
+    assert isinstance(mod.val_dataloader(), DataLoader)
+    assert isinstance(mod.test_dataloader(), DataLoader)
