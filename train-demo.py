0729962e337cd27687c7b0e850858fd07a37681c
Han Xiao <xiaohan2012@gmail.com>
2025-04-25 09:42:51 +0300

    train-demo.py

---
+++ train-demo.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from stgym.train import train
+from stgym.config_schema import (
+    MessagePassingConfig,
+    ModelConfig,
+    PoolingConfig,
+    PostMPConfig,
+    TrainConfig,
+    OptimizerConfig,
+    LRScheduleConfig,
+    DataLoaderConfig,
+)
+from stgym.data_loader import STDataModule
+from stgym.tl_model import STGymModule
+
+
+model_cfg = ModelConfig(
+    # message passing layers
+    mp_layers=[
+        MessagePassingConfig(
+            layer_type="gcnconv",
+            hidden_dim=64,
+            pooling=PoolingConfig(type="dmon", n_clusters=8),
+        ),
+        # # training works under one mp+pooling layer only, not more than that
+        # MessagePassingConfig(
+        #     layer_type="gcnconv",
+        #     pooling=PoolingConfig(type="dmon", n_clusters=4),
+        # ),
+    ],
+    global_pooling="mean",
+    post_mp_layer=PostMPConfig(dims=[16, 8]),
+)
+
+
+train_cfg = TrainConfig(
+    optim=OptimizerConfig(), lr_schedule=LRScheduleConfig(type=None), max_epoch=5
+)
+
+
+data_cfg = DataLoaderConfig(dataset_name="brca", batch_size=8)
+
+
+data_module = STDataModule(data_cfg)
+model_module = STGymModule(
+    dim_in=data_module.num_features,
+    dim_out=1,  # 1 for binary classification
+    model_cfg=model_cfg,
+    train_cfg=train_cfg,
+)
+print(model_module.model)
+
+
+train(model_module, data_module, train_cfg, trainer_config={"log_every_n_steps": 10})
