93a15992bfe8e84d0fa9f7efaf121ad99d303920
Han Xiao <xiaohan2012@gmail.com>
2025-04-30 09:48:05 +0300

    rct.run

---
+++ stgym/rct/run.py
@@ -0,0 +1,36 @@
+import ray
+
+from stgym.config_schema import ExperimentConfig
+from stgym.data_loader import STDataModule
+from stgym.rct.exp_gen import generate_experiment_configs, load_config
+from stgym.tl_model import STGymModule
+from stgym.train import train
+
+
+@ray.remote(num_cpus=2)
+def run_exp(cfg: ExperimentConfig):
+    data_module = STDataModule(cfg.task, cfg.data_loader)
+    model_module = STGymModule(
+        dim_in=data_module.num_features,
+        dim_out=1,  # 1 for binary classification
+        model_cfg=cfg.model,
+        train_cfg=cfg.train,
+    )
+    train(
+        model_module, data_module, cfg.train, trainer_config={"log_every_n_steps": 10}
+    )
+    return True
+
+
+def main():
+    rct_config_path = "./configs/rct/bn.yaml"
+    rct_config = load_config(rct_config_path)
+
+    exp_cfgs = generate_experiment_configs(rct_config)
+    promises = [run_exp.remote(cfg) for cfg in exp_cfgs]
+    results = ray.get(promises)
+    print(f"results: {results}")
+
+
+if __name__ == "__main__":
+    main()
