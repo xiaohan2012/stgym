13e16fd80039483f0de4674972dbc325c928ceeb
Han Xiao <xiaohan2012@gmail.com>
2025-06-11 09:22:26 +0300

    info fix

--- stgym/data_loader/ds_info.py
+++ stgym/data_loader/ds_info.py
@@ -5,7 +5,7 @@ __data__ = {
     "brca": {"max_span": 1018.48, "num_classes": 2},
     "human-crc": {"max_span": 1919.00, "num_classes": 10},
     "mouse-spleen": {"max_span": 1341.0, "num_classes": 58},
-    "mouse-preoptic": {"max_span": 9512.359375, "num_classes": 5},
+    "mouse-preoptic": {"max_span": 9512.359375, "num_classes": 6},
 }
 
 
