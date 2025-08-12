01eb7963965f9d833f4edab84101506a930a722e
Han Xiao <xiaohan2012@gmail.com>
2025-08-12 07:13:35 +0300

    config update

--- scripts/test_on_cpu.sh
+++ scripts/test_on_cpu.sh
@@ -4,6 +4,6 @@ python run_rct.py \
        +exp=bn \
        design_space=graph_clf \
        resource=cpu-4 \
-       sample_size=1 \
+       sample_size=2 \
        design_space.train.max_epoch=10 \
        mlflow.experiment_name=test
