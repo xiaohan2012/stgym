75ff3dda1238fbdf9446929ea596a005596eb223
Han Xiao <xiaohan2012@gmail.com>
2025-08-12 06:52:28 +0300

    increase sample size in test script

--- scripts/test_on_gpu.sh
+++ scripts/test_on_gpu.sh
@@ -4,6 +4,6 @@ python run_rct.py \
        +exp=bn \
        design_space=graph_clf \
        resource=gpu-2 \
-       sample_size=1 \
+       sample_size=5 \
        design_space.train.max_epoch=10 \
        mlflow.experiment_name=test
