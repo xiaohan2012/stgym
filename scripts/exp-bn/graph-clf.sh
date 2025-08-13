e57fceec78b90ec020c28687df7d766a5ad75333
Han Xiao <xiaohan2012@gmail.com>
2025-08-13 21:54:47 +0300

    bn exp for graph clf.

---
+++ scripts/exp-bn/graph-clf.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+time python run_rct.py \
+       +exp=bn \
+       design_space=graph_clf \
+       resource=gpu-2 \
+       sample_size=100 \
+       design_space.train.max_epoch=200 \
+       mlflow.experiment_name=graph-clf-aug-13-2025
