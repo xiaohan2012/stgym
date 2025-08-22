3792905b63f8b816716959f089a38cf99f558c34
Han Xiao <xiaohan2012@gmail.com>
2025-08-22 09:40:23 +0300

    minor

--- scripts/test_on_cpu.sh
+++ scripts/test_on_cpu.sh
@@ -4,6 +4,6 @@ python run_rct.py \
        +exp=bn \
        design_space=graph_clf \
        resource=cpu-4 \
-       sample_size=2 \
+       sample_size=1 \
        design_space.train.max_epoch=10 \
        mlflow.experiment_name=test
