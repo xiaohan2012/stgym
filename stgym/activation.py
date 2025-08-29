aebb7b0607d4703bbc7404f33f7f0b6e61153b7e
Han Xiao <xiaohan2012@gmail.com>
2025-08-29 06:58:58 +0300

    add swish act functon

--- stgym/activation.py
+++ stgym/activation.py
@@ -6,3 +6,7 @@ def get_activation_function(name):
         return torch.nn.PReLU()
     elif name == "relu":
         return torch.nn.ReLU()
+    elif name == "swish":
+        return torch.nn.SiLU()
+    else:
+        raise ValueError(f"Unknown activation function '{name}'")
