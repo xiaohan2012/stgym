389cf80d35ddea98a06c44c901d1f3e8003e7e25
Han Xiao <xiaohan2012@gmail.com>
2025-11-11 17:26:20 +0800

    change params in scripts

--- scripts/test_on_gpu.sh
+++ scripts/test_on_gpu.sh
@@ -4,6 +4,6 @@ time python run_rct.py \
        +exp=hpooling \
        design_space=graph_clf \
        resource=gpu-2 \
-       sample_size=10 \
-       design_space.train.max_epoch=10 \
+       sample_size=2 \
+       design_space.train.max_epoch=2 \
        mlflow.experiment_name=test
