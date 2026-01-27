f285e1d86210d7637c23515604afa96ae47e811f
Han Xiao <xiaohan2012@gmail.com>
2026-01-27 22:22:22 +0200

    exclude mouse_kidney for the time-being

--- stgym/data_loader/ds_info.py
+++ stgym/data_loader/ds_info.py
@@ -69,14 +69,14 @@ __data__ = {
         "data_source_url": "https://cellxgene.cziscience.com/collections/4195ab4c-20bd-4cd3-8b3d-65601277e731",
         "used_in_paper": "CellContrast: Reconstructing spatial relationships in single-cell RNA sequencing data via deep contrastive learning",
     },
-    DatasetName.mouse_kidney: {
-        "min_span": 4708.080078125,
-        "max_span": 5432.63720703125,
-        "num_classes": 3,
-        "task_type": "graph-classification",
-        "data_source_url": "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE190094",
-        "used_in_paper": "High-resolution Slide-seqV2 spatial transcriptomics enables discovery of disease-specific cell neighborhoods and pathways",
-    },
+    # DatasetName.mouse_kidney: {
+    #     "min_span": 4708.080078125,
+    #     "max_span": 5432.63720703125,
+    #     "num_classes": 3,
+    #     "task_type": "graph-classification",
+    #     "data_source_url": "https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE190094",
+    #     "used_in_paper": "High-resolution Slide-seqV2 spatial transcriptomics enables discovery of disease-specific cell neighborhoods and pathways",
+    # },
     DatasetName.cellcontrast_breast: {
         "min_span": 16.074260711669922,
         "max_span": 16.33220672607422,
