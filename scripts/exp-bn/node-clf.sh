8f39af68821f7306a048c8cbb97bb131738c6d81
Han Xiao <xiaohan2012@gmail.com>
2026-01-27 08:14:38 +0200

    use generic gpu resource for existing exp configs

--- scripts/exp-bn/node-clf.sh
+++ scripts/exp-bn/node-clf.sh
@@ -3,7 +3,7 @@
 time python run_rct.py \
        +exp=bn \
        design_space=node_clf \
-       resource=gpu-2 \
+       resource=gpu \
        sample_size=100 \
        design_space.train.max_epoch=200 \
        mlflow.experiment_name=node-clf-bn-$(date +%m-%d-%Y)
