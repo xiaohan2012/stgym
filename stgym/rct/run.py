8e02b8d00cc0626dcf1bd0d55c66302130e24725
Han Xiao <xiaohan2012@gmail.com>
2026-03-26 21:17:39 +0200

    minor

--- stgym/rct/run.py
+++ stgym/rct/run.py
@@ -46,9 +46,9 @@ def get_dim_out(task_cfg: TaskConfig) -> int | None:
 TL_TRAIN_CFG = {
     "log_every_n_steps": 5,
     # simplify the logging
-    "enable_progress_bar": True,
+    # "enable_progress_bar": True,
     # "enable_model_summary": True,
-    # "enable_progress_bar": False,
+    "enable_progress_bar": False,
     "enable_model_summary": False,
     "enable_checkpointing": False,
 }
